f665b6a5a91e5f5e1652f81f6513dc4477a7427c
utsavjainb <utsavjainb@gmail.com>
2020-06-10 11:22:44 -0700

    Updating structs file to current working version

--- FAstack_structs.h
+++ FAstack_structs.h
@@ -1,29 +1,28 @@
 #include <atomic>
 
-#define N 10 
-//#define MAX_FAILURES 0 
-//#define MAX_FAILURES 10 
-//int N = 10;
+#define N 120 
 
 struct Element {
     int e;
 };
 
-
 struct State {
     int pending{1};
     int id{63};
 };
-    
 
-struct PushReq {
+class PushReq {
+  public:
     Element elem;
-	std::atomic<State> state;
+    std::atomic<State> state;
+    PushReq() {}
 };
 
-struct PopReq {
+class PopReq {
+  public:
     int idx{64};
-	std::atomic<State> state;
+    std::atomic<State> state;
+    PopReq() {}
 };
 
 PushReq* uptickPush;
@@ -44,90 +43,33 @@ struct Cell {
     std::atomic<Element> elem;
     std::atomic<PushReq*> push;
     std::atomic<PopReq*> pop;
-	std::atomic<int> offset{64};
+    std::atomic<int> offset{64};
 };
 
-/*
-class Cell {
-  public:
-    std::atomic<Element> elem;
-    std::atomic<PushReq*> push;
-    std::atomic<PopReq*> pop;
-	std::atomic<int> offset;
-    Cell() {
-        offset = 64;
-    }
-}
-*/
 
 struct Segment {
     int id{64};
-    //int counter{64};
     std::atomic<int> counter{64};
     int time_stamp{64};
     bool retired;
-	std::atomic<Segment*> prev;
+    std::atomic<Segment*> prev;
     std::atomic<Segment*> next;
     std::atomic<Segment*> real_next; 
     std::atomic<Segment*> free_next;
-    //Cell* cells[4];
     Cell* cells[N];
 };
 
-/*
-class Segment {
-  public:
-    int id;
-    std::atomic<int> counter;
-    int time_stamp;
-    bool retired;
-	std::atomic<Segment*> prev;
-    std::atomic<Segment*> next;
-    std::atomic<Segment*> real_next; 
-    std::atomic<Segment*> free_next;
-    Cell* cells[4];
-    Segment() {
-        id = 64;
-        counter = 64;
-        time_stamp = 64;
-    }
-}
-*/
-
-/*
-struct Handle {
-    Segment* top;
-    Segment* sp; 
-    std::atomic<Segment*> free_list;
-    Handle* next;
-    struct {
-        int help_id{64};
-        PushReq req;
-        Handle *peer;
-    } push;
-
-    struct {
-        PopReq req;
-        Handle *peer;
-    } pop;
-    
-    int time_stamp{64}; 
-    
-};
-*/
-
 Segment* init_segment(int id){
-	Segment* sg = (Segment*) malloc(sizeof(Segment));
-	sg->id = id;
-	sg->counter = 0;
-	//sg->time_stamp = get_timestamp();
-	sg->time_stamp = uptickTime;
-	sg->retired = false;
-	sg->prev = NULL;
-	sg->next = NULL;
-	sg->real_next = NULL;
-	sg->free_next = NULL;
-	return sg;
+    Segment* sg = (Segment*) malloc(sizeof(Segment));
+    sg->id = id;
+    sg->counter = 0;
+    sg->time_stamp = uptickTime;
+    sg->retired = false;
+    sg->prev = NULL;
+    sg->next = NULL;
+    sg->real_next = NULL;
+    sg->free_next = NULL;
+    return sg;
 }
 
 Segment* new_segment(int id) {
@@ -140,8 +82,6 @@ Segment* new_segment(int id) {
     }   
     return sg; 
 }
-
-
 
 class Handle{
   public:
@@ -172,7 +112,8 @@ class Handle{
         time_stamp = 64;    
         top = new_segment(1);
         sp = new_segment(2);
-        //free_list.store(new_segment(3), std::memory_order_relaxed);
+        push.peer = this; 
+        pop.peer = this; 
     }
 }; 
 
@@ -190,6 +131,6 @@ void alloc_poppeer(Handle *h) {
 }
 
 struct Stack {
-	std::atomic<Segment*> top;   
+    std::atomic<Segment*> top;   
     std::atomic<int> T{ATOMIC_VAR_INIT(64)};
 };
